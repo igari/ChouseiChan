fe5c783ad1fa2c9c7e2929f9f3a11d1e9bca5f2a
igari <d.igari.takeharu@gmail.com>
2024-02-17 09:40:22 +0900

    Add emulators configuration and update CORS options

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,7 +28,7 @@ const app = initializeApp(firebaseConfig)
 
 const db = getFirestore(app)
 
-const options = { cors: ['https://itsusuru.com', 'http://localhost:3000'] }
+const options = { cors: ['https://itsusuru.com'] }
 
 export const createEvent = onRequest(
   options,
